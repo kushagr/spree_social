a942a144de2cc1a59743d0e8c3afae6573edd9bb
Kushagr <kushagra@s.treniq.com>
2015-06-30 21:16:28 +0530

    fixed issue where there was a conflict between spree_social and devise_invitable routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Spree::Core::Engine.routes.draw do
-  devise_for :spree_user,
-             :class_name => Spree::User,
-             :skip => [:unlocks, :sessions, :registrations, :passwords],
-             :controllers => { :omniauth_callbacks => "spree/omniauth_callbacks" },
-             :path => Spree::SocialConfig[:path_prefix]
+    devise_for :spree_user,
+             class_name: Spree::User,
+             only: [:omniauth_callbacks],
+             controllers: { omniauth_callbacks: 'spree/omniauth_callbacks' },
+             path: Spree::SocialConfig[:path_prefix]
   resources :user_authentications
 
   get 'account' => 'users#show', :as => 'user_root'
